13342aa38efd8b4bc5160070895afc5d866bf3a5
abigail <abigailwolfmoon@gmail.com>
2020-07-19 16:54:48 +0200

    Add a project and style the img

--- src/components/content/Projects.js
+++ src/components/content/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PortfolioImg from '../../static/img/portfolio_card_image.png';
 
 const projectsList = [
   {
-    name: 'Placeholder',
-    image: '',
+    name: 'Online Portfolio',
+    image: PortfolioImg,
     imagePlaceholder: 'Image',
-    description: 'This is just a placeholder',
-    icons: ['fab fa-python', 'fab fa-html5'],
+    description: 'This very portfolio',
+    icons: ['fab fa-react', 'fab fa-html5', 'fab fa-sass'],
   },
   {
     name: 'Placeholder',
@@ -27,17 +28,21 @@ const projectsList = [
 const Projects = () => {
   return (
     <div className="projects">
-      <h2 className="projects-header">Previous Projects</h2>
+      <h2 className="projects-header">Showcased Projects</h2>
       <div className="projects-list">
         {projectsList.map((p) => {
           return (
             <div className="project-card">
               <h4>{p.name}</h4>
-              <img src={p.image} placeholder={p.imagePlaceholder}></img>
+              <div className="img-container">
+                <img src={p.image} placeholder={p.imagePlaceholder}></img>
+              </div>
               <p>{p.description}</p>
-              {p.icons.map((i) => {
-                <i className={i}></i>;
-              })}
+              <div className="project-tech-list">
+                {p.icons.map((i) => {
+                  return <i className={i}></i>;
+                })}
+              </div>
             </div>
           );
         })}

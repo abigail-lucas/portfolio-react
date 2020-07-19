7b680c294e2c4427587dbb767dbf38d5a2db710a
abigail <abigailwolfmoon@gmail.com>
2020-07-19 14:43:06 +0200

    Add collapse button and animation to the skills section

--- src/components/content/info/Skills.js
+++ src/components/content/info/Skills.js
@@ -1,50 +1,95 @@
 import React from 'react';
 
+const hardSkills = [
+  {
+    name: 'Front End',
+    items: [
+      {
+        name: 'HTML / CSS',
+        experience: '4 years',
+      },
+      {
+        name: 'JavaScript',
+        experience: '4 years',
+      },
+      {
+        name: 'React',
+        experience: '1 years',
+      },
+      {
+        name: 'Other Libraries',
+        experience: '2 years',
+      },
+    ],
+  },
+  {
+    name: 'Back End',
+    items: [
+      {
+        name: 'Python',
+        experience: '4 years',
+      },
+      {
+        name: 'Django',
+        experience: '4 years',
+      },
+      {
+        name: 'Database Design',
+        experience: '3 years',
+      },
+    ],
+  },
+  {
+    name: 'Other Skills',
+    items: [
+      {
+        name: 'UI/UX Design',
+        experience: '~',
+      },
+      {
+        name: 'Project Management',
+        experience: '~',
+      },
+    ],
+  },
+];
+
+const operateList = (event) => {
+  const target = event.target.dataset.target;
+  const list = document.getElementById(target);
+  if (list.classList.contains('flat')) {
+    list.classList.remove('flat');
+  } else {
+    list.classList.add('flat');
+  }
+};
+
 const Skills = () => {
   return (
     <div className="skill-container">
-      <ul className="skills-list">
-        <li className="skill-header">Front End</li>
-        <li className="skill-item">
-          <div>HTML / CSS</div>
-          <div>4 years</div>
-        </li>
-        <li className="skill-item">
-          <div>JavaScript</div>
-          <div>4 years</div>
-        </li>
-        <li className="skill-item">
-          <div>React</div>
-          <div>1 years</div>
-        </li>
-        <li className="skill-item">
-          <div>Other Libraries</div>
-          <div>2 years</div>
-        </li>
-
-        <li className="skill-header">Backend End</li>
-        <li className="skill-item">
-          <div>Python</div>
-          <div>4 years</div>
-        </li>
-        <li className="skill-item">
-          <div>Django</div>
-          <div>4 years</div>
-        </li>
-        <li className="skill-item">
-          <div>Database Design</div>
-          <div>3 years</div>
-        </li>
-
-        <li className="skill-header">Other</li>
-        <li className="skill-item">
-          <div>UI/UX Design</div>
-          <div>~</div>
-        </li>
-        <li className="skill-item">
-          <div>Project Management</div>
-          <div>~</div>
-        </li>
+      <h4
+        className="skills-container-header"
+        data-target="hard-skills"
+        onClick={operateList}
+      >
+        <span data-target="hard-skills"></span>Hard Skills
+      </h4>
+      <ul className="skills-list flat" id="hard-skills">
+        {hardSkills.map((s) => {
+          return (
+            <React.Fragment>
+              <li className="skill-header">{s.name}</li>
+              {s.items.map((i) => {
+                return (
+                  <li className="skill-item">
+                    <div>{i.name}</div>
+                    <div>{i.experience}</div>
+                  </li>
+                );
+              })}
+            </React.Fragment>
+          );
+        })}
       </ul>
     </div>
   );
